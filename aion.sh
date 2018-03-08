11d5c1f23a06cc4d0074e66196d883d605c39f3a
Alin Turbut <alin.turbut@iwelcome.com>
2018-03-08 21:39:01 +0200

    First version of gradle

--- aion.sh
+++ aion.sh
@@ -14,15 +14,15 @@ if [ "$HW" != "x86_64" ]; then
   echo "Warning! Aion blockchain platform must be running on the 64 bits architecture"
 fi
 
-DIST=$(lsb_release -i | grep -o "Ubuntu")
+DIST=$(uname -a | grep -o "Ubuntu")
 
 if [ "$DIST" != "Ubuntu" ]; then
-  echo "Warning! Aion blockchain is fully compatible with the Ubuntu distribution. Your current system is not Ubuntu distribution. It may has some issues."
+  echo "Warning! Aion blockchain is fully compatible with the Ubuntu distribution. Your current system is not Ubuntu distribution. It may have some issues."
 fi
 
-MAJVER=$(lsb_release -r | grep -o "[0-9][0-9]" | sed -n 1p)
+MAJVER=$(uname -r | grep -o "[0-9][0-9]" | sed -n 1p)
 if [ "$MAJVER" -lt "16" ]; then
-  echo "Warning! Aion blockchain is fully compatible with the Ubuntu version 16.04. Your current system is older than Ubuntu 16.04. It may has some issues."
+  echo "Warning! Aion blockchain is fully compati ble with the Ubuntu version 16.04. Your current system is older than Ubuntu 16.04. It may have some issues."
 fi
 
 ARG=$@
